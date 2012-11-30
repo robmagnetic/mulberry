15df69769a289b0a18905597036a2b8b21403bab
Dan Imal <dimal@toura.com>
2012-11-30 17:35:01 +0000

    add documentation for BackgroundImage

--- app/mulberry/ui/BackgroundImage.js
+++ app/mulberry/ui/BackgroundImage.js
@@ -1,5 +1,14 @@
 dojo.provide('mulberry.ui.BackgroundImage');
 
+/**
+ * This provides a simple interface for rendering an image in a way that it
+ * will not cause a memory leak. <img> tags never release their memory on
+ * mobile webkit, even when they are removed from the DOM. Instead, we render
+ * the image as a CSS background image, and provide an interface to load or
+ * unload the image. This lets us unload the image if the image if it outside
+ * the viewport.
+ */
+
 dojo.require('dijit._Widget');
 
 dojo.declare('mulberry.ui.BackgroundImage', dijit._Widget, {
